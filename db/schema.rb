7aa67d3b2380c38935746ed0a73bc28bab4b832a
damon johnson <damon-johnson@outlook.com>
2022-05-12 21:23:10 +1000

    added destroy dependent :destroy to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_12_110311) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_12_111618) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,9 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_12_110311) do
     t.datetime "updated_at", null: false
     t.bigint "category_id", null: false
     t.bigint "user_id", null: false
-    t.bigint "location_id", null: false
     t.index ["category_id"], name: "index_items_on_category_id"
-    t.index ["location_id"], name: "index_items_on_location_id"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
@@ -70,7 +68,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_12_110311) do
   end
 
   add_foreign_key "items", "categories"
-  add_foreign_key "items", "locations"
   add_foreign_key "items", "users"
   add_foreign_key "profiles", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,24 +18,20 @@ fiona = User.create(email: 'fiona@email.com', password: 'password')
 randal= User.create(email: 'randal@email.com', password: 'password')
 greg = User.create(email: 'greg@email.com', password: 'password')
 
-christine.profiles.create(first_name: 'christine', last_name: 'jones', address: "98 Chickiba Drive", city: 'East Ballina', postcode: 2478, state: 'New South Wales')
-
-
-
-# location1 = Location.create(address: "123 Foo Street", city: 'Brisbane', postcode: 4000, state: 'Queensland')
-# location2 = Location.create(address: "67 Baroona Road", city: 'Milton', postcode: 4067, state: 'Queensland')
-# location3 = Location.create(address: "98 Chickiba Drive", city: 'East Ballina', postcode: 2478, state: 'New South Wales')
-# location4 = Location.create(address: "4 Collins Street", city: 'Docklands', postcode: 3005, state: 'Victoria')
+Profile.create(user_id: User.first.id, first_name: 'christine', last_name: 'jones', address: "98 Chickiba Drive", city: 'East Ballina', postcode: 2478, state: 'New South Wales')
+Profile.create(user_id: User.second.id, first_name: 'fiona', last_name: 'bakhteyari', address: "67 Baroona Road", city: 'Milton', postcode: 4067, state: 'Queensland')
+Profile.create(user_id: User.third.id, first_name: 'randal', last_name: 'johnson', address: "123 Foo Street", city: 'Brisbane', postcode: 4000, state: 'Queensland')
+Profile.create(user_id: User.last.id, first_name: 'greg', last_name: 'simpson', address: "4 Collins Street", city: 'Docklands', postcode: 3005, state: 'Victoria')
 
 
 
 
-# john_profile = Profile.create(user_id: User.first.id, first_name: 'John', last_name: 'Doe')
 
-# categories = Category.create([{name: "4WDs"},{name: "Campervans"},{name: "Caravans"},{name: "Kayaks and Canoes"},{name: "Boats"},{name: "Camping Equipment"},{name: "Surf"},{name: "Snow"}])
 
-# canoe = Item.create(title: "Two person canoe", description: "A ten foot traditional canadian canoe with room for two people and gear storage in the middle. Comes with two paddles.", price: 59.00, category_id: Category.first.id, user_id: User.first.id, location_id: Location.first.id)
-# Item.create(title: "Landrover Defender", description: "A 2017 troop carrier land rover defender - manual 4x4, 4 cylinder 2.2L turbo diesel.", price: 145.00, category_id: Category.find_by(name: "4WDs").id, user_id: User.second.id)
-# tent = Item.create(title: "Denali single person hiking tent", description: "A rarely used light weight hiking tent. Weight is 2.5kg", price: 30.00, category_id: Category.find_by(name: "Camping Equipment").id, user_id: User.second.id)
-# paddleboard = Item.create(title: "Standup paddleboard", description: "A twelve foot epoxy paddleboard. Come with paddle.", price: 72.00, category_id: Category.find_by(name: "Surf").id, user_id: User.third.id)
-# Item.create(title: 'Foo', description: 'Bar', price: 100.00, category_id: Category.first.id, user_id: User.fourth.id)
+categories = Category.create([{name: "4WDs"},{name: "Campervans"},{name: "Caravans"},{name: "Kayaks and Canoes"},{name: "Boats"},{name: "Camping Equipment"},{name: "Surf"},{name: "Snow"}])
+
+canoe = Item.create(title: "Two person canoe", description: "A ten foot traditional canadian canoe with room for two people and gear storage in the middle. Comes with two paddles.", price: 59.00, category_id: Category.first.id, user_id: User.first.id)
+Item.create(title: "Landrover Defender", description: "A 2017 troop carrier land rover defender - manual 4x4, 4 cylinder 2.2L turbo diesel.", price: 145.00, category_id: Category.find_by(name: "4WDs").id, user_id: User.second.id)
+tent = Item.create(title: "Denali single person hiking tent", description: "A rarely used light weight hiking tent. Weight is 2.5kg", price: 30.00, category_id: Category.find_by(name: "Camping Equipment").id, user_id: User.last.id)
+paddleboard = Item.create(title: "Standup paddleboard", description: "A twelve foot epoxy paddleboard. Come with paddle.", price: 72.00, category_id: Category.find_by(name: "Surf").id, user_id: User.third.id)
+Item.create(title: 'Foo', description: 'Bar', price: 100.00, category_id: Category.first.id, user_id: User.last.id)
