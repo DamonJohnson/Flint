f2b4c56eb53483994f6665e68f57a7d06efef553
damon johnson <damon-johnson@outlook.com>
2022-05-16 11:18:32 +1000

    created devise users controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+
+   
 Rails.application.routes.draw do
-  devise_for :users
-  
+  devise_for :users, controllers: { sessions: "users/sessions" }
+
   get 'profiles/new', to: 'profiles#new', as: 'profiles'
   get 'profiles/:id', to: 'profiles#show', as: 'profile'
   get 'profiles/:id/edit', to: 'profiles#edit', as: 'edit_profile'
@@ -8,25 +10,30 @@ Rails.application.routes.draw do
   post 'profiles/new', to: 'profiles#create'
   delete 'profiles/:id', to: 'profiles#destroy'
 
-
-
   get 'items', to: 'items#index', as: 'items'
   get 'items/new', to: 'items#new', as: 'new'
   get 'items/:id', to: 'items#show', as: 'item'
   post 'items/new', to: 'items#create'
-<<<<<<< HEAD
-  get 'items/edit/:id', to: 'items#edit', as: 'edit_item'
-=======
   get 'items/:id/edit', to: 'items#edit', as: 'edit_item'
->>>>>>> master
   patch 'items/:id', to: 'items#update'
   delete 'items/:id', to: 'items#destroy'
-  
-  # devise_for :users
-  get 'pages/index'  
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
+  # Bookings where user is lendee
+  get 'lendee/:id/bookings', to: 'bookings#index', as: 'lendee_bookings'
+  get 'items/:id/bookings/new', to: 'bookings#new', as: 'new_booking'
+  post 'items/:id/bookings', to: 'bookings#create'
+  get 'bookings/:id/show', to: 'bookings#show', as: 'booking'
+  get 'bookings/:id/edit', to: 'bookings#edit'
+  patch 'bookings/:id/', to: 'bookings#update'
+  delete 'items/:id', to: 'bookings#destroy'
+
+  # Bookings where user is item owner
+  get 'items/:id/bookings', to: 'bookings#item_bookings', as: 'item_bookings'
+
+  post 'lendees/create', to: 'lendees#create', as: 'lendee_create'
+
+  get 'pages/index'  
+
   root "pages#index"
 end
  
--- db/migrate/20220515053308_create_lendees.rb
+++
@@ -1,9 +0,0 @@
-class CreateLendees < ActiveRecord::Migration[7.0]
-  def change
-    create_table :lendees do |t|
-      t.references :user, null: false, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
